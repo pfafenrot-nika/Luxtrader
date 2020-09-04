a5625f32b7f655ecb18ad04a720a27f5e2992358
Pfafenrot Veronika <pfafenrot_nika@mail.ru>
2020-09-04 13:45:36 +0300

    Готов блок 'Лоты'

--- #src/js/sliders.js
+++ #src/js/sliders.js
@@ -1,4 +1,5 @@
 //SLIDERS
+// main-slider
 if($('.main-slider__body').length>0){
 	$('.main-slider__body').slick({
 		adaptiveHeight: true, 
@@ -18,6 +19,46 @@ if($('.main-slider__body').length>0){
 			settings: {
 			}
 		}]
+	});
+}
+
+// lots__slider
+if($('.slider-lots__body').length>0){
+	$('.slider-lots__body').slick({
+		adaptiveHeight: true, 
+		draggable: true,
+		//autoplay: true,
+		infinite: true,
+		//dots: true,
+		arrows: true,
+		//accessibility: false,
+		slidesToShow: 3,
+		//autoplaySpeed: 5000,
+		speed: 1000,
+		appendArrows:$('.slider-lots__control'),
+		nextArrow:'<div class="control-slider-lots__arrow control-slider-lots__arrow_prev"><svg xmlns="http://www.w3.org/2000/svg" width="109" height="36" viewBox="0 0 109 36"><defs><clipPath id="enpwa"><path fill="#fff" d="M90.615 1.03l.707-.708 16.97 16.971L109 18l-.707.707-16.97 16.97-.708-.706L107.585 18z"/></clipPath><clipPath id="enpwb"><path fill="#fff" d="M83.615 1.03l.707-.708 16.97 16.971L102 18l-.707.707-16.97 16.97-.708-.706L100.585 18z"/></clipPath><clipPath id="enpwc"><path fill="#fff" d="M0 18v-1h108v1z"/></clipPath></defs><g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="2" d="M90.615 1.03l.707-.708 16.97 16.971h0L109 18l-.707.707h0l-16.97 16.97-.708-.706L107.585 18z" clip-path="url(&quot;#enpwa&quot;)"/></g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="2" d="M83.615 1.03l.707-.708 16.97 16.971h0L102 18l-.707.707h0l-16.97 16.97-.708-.706L100.585 18z" clip-path="url(&quot;#enpwb&quot;)"/></g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="6" d="M0 18v0-1 0h108v1z" clip-path="url(&quot;#enpwc&quot;)"/></g></g></svg></div>',
+		prevArrow:'<div class="control-slider-lots__arrow control-slider-lots__arrow_next"><svg xmlns="http://www.w3.org/2000/svg" width="108" height="35" viewBox="0 0 108 35"><defs><clipPath id="3ekwa"><path fill="#fff" d="M.707 18.2L0 17.5l.707-.7L17.677.01l.708.7L1.415 17.5l16.97 16.79-.707.7L.708 18.2z"/></clipPath><clipPath id="3ekwb"><path fill="#fff" d="M24.678.01l.707.7L8.415 17.5l16.97 16.79-.707.7L7 17.5z"/></clipPath><clipPath id="3ekwc"><path fill="#fff" d="M108 17.5v-.99H1v.99z"/></clipPath></defs><g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="6" d="M.707 18.2L0 17.5l.707-.7h0L17.677.01l.708.7L1.415 17.5l16.97 16.79-.707.7L.708 18.2z" clip-path="url(&quot;#3ekwa&quot;)"/></g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="6" d="M24.678.01l.707.7L8.415 17.5l16.97 16.79-.707.7L7 17.5z" clip-path="url(&quot;#3ekwb&quot;)"/></g><g><path fill="none" stroke="#bb9c66" stroke-miterlimit="20" stroke-width="6" d="M108 17.5v0-.99 0H1v.99z" clip-path="url(&quot;#3ekwc&quot;)"/></g></g></svg></div>',
+		mobileFirst: true,
+		responsive: [
+			{
+				breakpoint: 320,
+				settings: {
+					slidesToShow: 1
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3
+				}
+			}
+		]
 	});
 }
 /*
