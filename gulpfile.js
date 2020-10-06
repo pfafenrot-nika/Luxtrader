4b270616c7a518b619618fa356831dbd930c4614
Pfafenrot Veronika <pfafenrot_nika@mail.ru>
2020-10-07 00:35:34 +0300

    Переименование папки для релиза (production)

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 
-let project_folder = require("path").basename(__dirname);
+let project_folder = require("path").basename(__dirname + '_production');
 let source_folder = "#src";
 
 let fs = require('fs');
